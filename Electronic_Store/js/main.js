da5cfa212816c52618b6ae6bc325051d4ddeb33b
DEVANG2502 <72226354+DEVANG2502@users.noreply.github.com>
2023-09-17 23:14:58 +0530

    Add files via upload
    Electronic Store

---
+++ Electronic_Store/js/main.js
@@ -0,0 +1,277 @@
+import prodb, {
+  bulkcreate,
+  createEle,
+  getData,
+  SortObj
+} from "./module.js";
+
+
+let db = prodb("Productdb", {
+  products: `++id, name, seller, price`
+});
+
+// input tags
+const userid = document.getElementById("userid");
+const proname = document.getElementById("proname");
+const seller = document.getElementById("seller");
+const price = document.getElementById("price1");
+
+// create button
+const btncreate = document.getElementById("btn-create");
+const btnread = document.getElementById("btn-read");
+const btnupdate = document.getElementById("btn-update");
+const btndelete = document.getElementById("btn-delete");
+
+// user data
+
+// event listerner for create button
+btncreate.onclick = event => {
+  // insert values
+  let flag = bulkcreate(db.products, {
+    name: proname.value,
+    seller: seller.value,
+    price: price.value
+  });
+  // reset textbox values
+  //proname.value = "";
+  //seller.value = "";
+  // price.value = "";
+  proname.value = seller.value = price.value = "";
+
+  // set id textbox value
+  getData(db.products, data => {
+    userid.value = data.id + 1 || 1;
+  });
+  table();
+
+  let insertmsg = document.querySelector(".insertmsg");
+  getMsg(flag, insertmsg);
+};
+
+// event listerner for create button
+btnread.onclick = table;
+
+// button update
+btnupdate.onclick = () => {
+  const id = parseInt(userid.value || 0);
+  if (id) {
+    // call dexie update method
+    db.products.update(id, {
+      name: proname.value,
+      seller: seller.value,
+      price: price.value
+    }).then((updated) => {
+      // let get = updated ? `data updated` : `couldn't update data`;
+      let get = updated ? true : false;
+
+      // display message
+      let updatemsg = document.querySelector(".updatemsg");
+      getMsg(get, updatemsg);
+
+      proname.value = seller.value = price.value = "";
+      //console.log(get);
+    })
+  } else {
+    console.log(`Please Select id: ${id}`);
+  }
+}
+
+// delete button
+btndelete.onclick = () => {
+  db.delete();
+  db = prodb("Productdb", {
+    products: `++id, name, seller, price`
+  });
+  db.open();
+  table();
+  textID(userid);
+  // display message
+  let deletemsg = document.querySelector(".deletemsg");
+  getMsg(true, deletemsg);
+}
+
+window.onload = event => {
+  // set id textbox value
+  textID(userid);
+};
+
+
+
+
+// create dynamic table
+function table() {
+  const tbody = document.getElementById("tbody");
+  const notfound = document.getElementById("notfound");
+  notfound.textContent = "";
+  // remove all childs from the dom first
+  while (tbody.hasChildNodes()) {
+    tbody.removeChild(tbody.firstChild);
+  }
+
+
+  getData(db.products, (data, index) => {
+    if (data) {
+      createEle("tr", tbody, tr => {
+        for (const value in data) {
+          createEle("td", tr, td => {
+            td.textContent = data.price === data[value] ? `$ ${data[value]}` : data[value];
+          });
+        }
+        createEle("td", tr, td => {
+          createEle("i", td, i => {
+            i.className += "fas fa-edit btnedit";
+            i.setAttribute(`data-id`, data.id);
+            // store number of edit buttons
+            i.onclick = editbtn;
+          });
+        })
+        createEle("td", tr, td => {
+          createEle("i", td, i => {
+            i.className += "fas fa-trash-alt btndelete";
+            i.setAttribute(`data-id`, data.id);
+            // store number of edit buttons
+            i.onclick = deletebtn;
+          });
+        })
+      });
+    } else {
+      notfound.textContent = "No record found in the database...!";
+    }
+
+  });
+}
+
+const editbtn = (event) => {
+  let id = parseInt(event.target.dataset.id);
+  db.products.get(id, function (data) {
+    let newdata = SortObj(data);
+    userid.value = newdata.id || 0;
+    proname.value = newdata.name || "";
+    seller.value = newdata.seller || "";
+    price.value = newdata.price || "";
+  });
+}
+
+// delete icon remove element 
+const deletebtn = event => {
+  let id = parseInt(event.target.dataset.id);
+  db.products.delete(id);
+  table();
+}
+
+// textbox id
+function textID(textboxid) {
+  getData(db.products, data => {
+    textboxid.value = data.id + 1 || 1;
+  });
+}
+
+// function msg
+function getMsg(flag, element) {
+  if (flag) {
+    // call msg 
+    element.className += " movedown";
+
+    setTimeout(() => {
+      element.classList.forEach(classname => {
+        classname == "movedown" ? undefined : element.classList.remove('movedown');
+      })
+    }, 4000);
+  }
+}
+
+$(".rating-component .star").on("mouseover", function () {
+  var onStar = parseInt($(this).data("value"), 10); //
+  $(this).parent().children("i.star").each(function (e) {
+    if (e < onStar) {
+      $(this).addClass("hover");
+    } else {
+      $(this).removeClass("hover");
+    }
+  });
+}).on("mouseout", function () {
+  $(this).parent().children("i.star").each(function (e) {
+    $(this).removeClass("hover");
+  });
+});
+
+$(".rating-component .stars-box .star").on("click", function () {
+  var onStar = parseInt($(this).data("value"), 10);
+  var stars = $(this).parent().children("i.star");
+  var ratingMessage = $(this).data("message");
+
+  var msg = "";
+  if (onStar > 1) {
+    msg = onStar;
+  } else {
+    msg = onStar;
+  }
+  $('.rating-component .starrate .ratevalue').val(msg);
+  
+
+ 
+  $(".fa-smile-wink").show();
+  
+  $(".button-box .done").show();
+
+  if (onStar === 5) {
+    $(".button-box .done").removeAttr("disabled");
+  } else {
+    $(".button-box .done").attr("disabled", "true");
+  }
+
+  for (i = 0; i < stars.length; i++) {
+    $(stars[i]).removeClass("selected");
+  }
+
+  for (i = 0; i < onStar; i++) {
+    $(stars[i]).addClass("selected");
+  }
+
+  $(".status-msg .rating_msg").val(ratingMessage);
+  $(".status-msg").html(ratingMessage);
+  $("[data-tag-set]").hide();
+  $("[data-tag-set=" + onStar + "]").show();
+});
+
+$(".feedback-tags  ").on("click", function () {
+  var choosedTagsLength = $(this).parent("div.tags-box").find("input").length;
+  choosedTagsLength = choosedTagsLength + 1;
+
+  if ($(this).hasClass("choosed")) {
+    $(this).removeClass("choosed");
+    choosedTagsLength = choosedTagsLength - 2;
+  } else {
+    $(this).addClass("choosed");
+    $(".button-box .done").removeAttr("disabled");
+  }
+
+  console.log(choosedTagsLength);
+
+  if (choosedTagsLength <= 0) {
+    $(".button-box .done").attr("enabled", "false");
+  }
+});
+
+
+
+$(".compliment-container .fa-smile-wink").on("click", function () {
+  $(this).fadeOut("slow", function () {
+    $(".list-of-compliment").fadeIn();
+  });
+});
+
+
+
+$(".done").on("click", function () {
+  $(".rating-component").hide();
+  $(".feedback-tags").hide();
+  $(".button-box").hide();
+  $(".submited-box").show();
+  $(".submited-box .loader").show();
+
+  setTimeout(function () {
+    $(".submited-box .loader").hide();
+    $(".submited-box .success-message").show();
+  }, 1500);
+});
